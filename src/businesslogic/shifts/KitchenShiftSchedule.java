091d240b76a5b94f93216212bc203ecaf7da36fd
emanuele <me@emanuele.xyz>
2021-05-31 12:24:17 +0200

    implement getKitchenShiftsSchedule

---
+++ src/businesslogic/shifts/KitchenShiftSchedule.java
@@ -0,0 +1,4 @@
+package businesslogic.shifts;
+
+public class KitchenShiftSchedule {
+}
---
+++ src/businesslogic/shifts/ShiftsManager.java
@@ -0,0 +1,9 @@
+package businesslogic.shifts;
+
+public class ShiftsManager {
+
+    // TODO: to be implemented
+    public KitchenShiftSchedule getKitchenShiftsSchedule() {
+        return null;
+    }
+}
