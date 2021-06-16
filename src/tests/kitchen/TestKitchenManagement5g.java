36ae2e852809e52fc14f2dc72875bf6dbfe505ed
emanuele <me@emanuele.xyz>
2021-06-16 11:58:51 +0200

    add tests templates

---
+++ src/tests/kitchen/TestKitchenManagement5g.java
@@ -0,0 +1,27 @@
+package tests.kitchen;
+
+import businesslogic.CatERing;
+import businesslogic.UseCaseLogicException;
+import businesslogic.kitchen.KitchenTaskException;
+
+public class TestKitchenManagement5g {
+    public static void main(String[] args) {
+        try {
+
+            // ---------------------------------------------------------------------------------------------------------
+
+            System.out.println("-".repeat(80));
+            System.out.println("TEST FAKE LOGIN");
+            System.out.println("-".repeat(80));
+
+            CatERing.getInstance().getUserManager().fakeLogin("Lidia");
+            System.out.println(CatERing.getInstance().getUserManager().getCurrentUser());
+
+            // ---------------------------------------------------------------------------------------------------------
+
+        } catch (KitchenTaskException | UseCaseLogicException e) {
+            System.out.println("Errore di logica nello use case");
+            e.printStackTrace();
+        }
+    }
+}
